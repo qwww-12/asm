56ca0cd4f23a420afdcd245094b275d941fc700a
qwww-12 <laro1laro34@gmail.com>
2025-07-25 11:17:16 +0100

    asm

--- start/f1.asm
+++ start/f1.asm
@@ -6,10 +6,10 @@ section	.text
 
 _start:
 	mov rax, 60
-	; mov rdi, [var] in memory rdi = 0x??????????????|09 -> this is a byte value of var and var is defined by byte so in another byte value is not found so we get on a garbage value
+	; mov rdi, [var] in memory rdi = 0x??????????????|09 -> this is a byte value of var and var is defined by byte so in another byte value is not found so we get on a garbage value but we can use normal mov
 
 	; solution we need use movzx move zero extension
-	mov rdi, [var]
+	movzx rdi, byte [var]
 	; now rdi = 0x0000000000000009
 	syscall
 
