59a229e0b67ae002eed32d1c742cb9884ba1b425
qwww-12 <laro1laro34@gmail.com>
2025-07-25 11:13:13 +0100

    asm

---
+++ start/f1.asm
@@ -0,0 +1,15 @@
+section	.data
+	var db 9
+
+section	.text
+	global _start
+
+_start:
+	mov rax, 60
+	; mov rdi, [var] in memory rdi = 0x??????????????|09 -> this is a byte value of var and var is defined by byte so in another byte value is not found so we get on a garbage value
+
+	; solution we need use movzx move zero extension
+	mov rdi, [var]
+	; now rdi = 0x0000000000000009
+	syscall
+
