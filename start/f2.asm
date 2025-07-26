cc31ffac4c3b177f63607d4b9f976b2522585210
qwww-12 <laro1laro34@gmail.com>
2025-07-26 13:27:13 +0100

    asm

---
+++ start/f2.asm
@@ -0,0 +1,16 @@
+section	.text
+	global _start
+_start:
+	push 21
+	push 1
+	call _21
+	add rsp, 16
+	mov rax, 60
+	syscall
+_21:
+	push rbp
+	mov rbp, rsp
+	mov rdi, [rbp + 16]
+	add rdi, [rbp + 24]
+	pop rbp
+	ret
